439a31a00f573bcc80fc36a7819b435b85630565
Suraj Yadav <surajyadav.tpa@jiostar.com>
2025-03-21 23:38:13 +0530

    JWT Token Creation

---
+++ JWTTokenBasedAuthDotNetAPI/APIController/UserAuthAPIController.cs
@@ -0,0 +1,37 @@
+﻿using JWTTokenBasedAuthDotNetAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTTokenBasedAuthDotNetAPI.APIController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthAPIController : ControllerBase
+    {
+        [HttpGet("AuthUser")]
+        public IActionResult AuthUser(string UserName, string Password)
+        {
+            List<User> users = GetUserDetails();
+
+            if(users.Exists(x => x.UserName == UserName && x.Password == Password))
+            {
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+
+        private static List<User> GetUserDetails()
+        {
+            return new List<User>
+            {
+                new User {  UserID = 1, UserName = "surajy", Password = "123" },
+                new User { UserID = 2, UserName = "vikasp", Password = "456" },
+                new User { UserID = 3, UserName = "nitinp", Password = "789" }
+            };
+        }
+    }
+}
